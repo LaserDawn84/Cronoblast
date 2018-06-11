7f0e48cdd4427aced0da09824d9b98f9f5885215
Lochlan Kennedy <38372089+Pharah-tato@users.noreply.github.com>
2018-06-11 22:52:56 +0800

    Master Update

---
+++ Assets/Scripts/EnemyMovement/Enemy1Walk.cs
@@ -0,0 +1,68 @@
+﻿/*/
+ *  Author: Lochlan Kennedy
+ *  Date: 11/06/2018
+ *  Version Control Repo: https://github.com/Pharah-tato/Cronoblast
+/*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer), typeof(Animator), typeof(Rigidbody2D))]
+public class Enemy1Walk : MonoBehaviour
+{
+    Animator enemyAnimator1; //Used to store the animator component
+    Vector2 currentPosition; //A vector 2 to store current position
+                             // Vector2 player2Position; //Will Sort this out if time to add multiplayer is sufficient
+                         
+    Vector2 playersPosition; // Store the player's position. Will be using this with 'currentPosition' to calculate the direction in which the enemy must move.
+                             //bool isCoOp = false; // check will be used if I add local multiplayer (Co-Op Single Screen).
+
+    Vector2 distanceFromPlayerToEnemy; //stores the distance to target.
+    Rigidbody2D rigB2DEnemy; //stores the rigidbody for the enemy
+    SpriteRenderer spriteRenderer; //stores the sprite renderer
+
+    [SerializeField]
+    GameObject player; // stores the player game object
+
+    [SerializeField]
+    GameObject player2; //stores GameObject for Player 2 (for Later Implementation of Co-Op)
+
+    float distanceMagnitude;
+
+    // Use this for initialization
+    void Start()
+    {
+        enemyAnimator1.GetComponent<Animator>();
+        spriteRenderer.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Movement()
+    {
+
+        currentPosition = new Vector2(transform.position.x,transform.position.y);
+        playersPosition = player.transform.position;
+
+        distanceFromPlayerToEnemy = playersPosition - currentPosition;
+        distanceMagnitude = distanceFromPlayerToEnemy.magnitude;
+
+        if(distanceMagnitude < 0)
+        {
+            spriteRenderer.flipX = true;
+            enemyAnimator1.SetFloat("Speed", distanceMagnitude); // sets the speed parameter for the animator to either set it to idle or walk.
+        }
+        else
+        {
+            spriteRenderer.flipX = false; // if
+            enemyAnimator1.SetFloat("Speed", distanceMagnitude); // sets the speed parameter for the animator to either set it to idle or walk.
+        }
+        //playersPosition = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical")); //sets the current position to the current player's input
+        //player2Position = newVector2(Input.GetAxis("P2Horizontal"),Input.GetAxis("P2Vertical"));
+        //These are place holder logics the actual movement will be a shared variable from character movement scripts.
+    }
+}
---
+++ Assets/Scripts/Player Character Scripts/CharacterMovement.cs
@@ -0,0 +1,75 @@
+﻿/*/
+ *  Author: Lochlan Kennedy
+ *  Date: 11/06/2018
+ *  Version Control Repo: https://github.com/Pharah-tato/Cronoblast
+/*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//THIS SCRIPT IS FOR PLAYER 1 WHEN IN Co-Op MODE (Didn't refractor script/class name due to potential screw ups with unity saving me from having to restart the project)
+
+[RequireComponent(typeof(Rigidbody2D),typeof(SpriteRenderer))]// requires the parent object to have components of SpriteRenderer and Rigidbody2D
+public class CharacterMovement : MonoBehaviour {
+
+    [SerializeField]
+    bool isUsingMouse = false;
+
+    Vector3 keyboardAndMouseInput = Vector3.zero; // for keyboard and mouse
+    Vector3 previousKeyboardAndMouseInput = Vector3.zero; //
+    Vector3 gamepadController1Input = Vector3.zero; // gamepad for player 1
+    //Vector3 gamepadController2Input = Vector3.zero; // gamepad for player 2
+    Vector3 mouseAim = Vector3.zero;
+
+    
+
+    [SerializeField]
+    float movementSpeed = 4f;
+
+    Rigidbody2D rigB2D;
+
+    [SerializeField]
+    GameObject gun;
+
+	// Use this for initialization
+	void Start () {
+        previousKeyboardAndMouseInput = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+	}
+
+    void Movement()
+    {
+        keyboardAndMouseInput = new Vector3(Input.GetAxis("KeyboardHorizontal"), Input.GetAxis("KeyboardVertical"),0f);
+        gamepadController1Input = new Vector3(Input.GetAxis("HorizontalP1"), Input.GetAxis("VerticalP1"), 0f);
+
+        if(keyboardAndMouseInput != previousKeyboardAndMouseInput)
+        {
+            isUsingMouse = true;
+            previousKeyboardAndMouseInput = keyboardAndMouseInput;
+            
+        }
+
+        if(keyboardAndMouseInput == previousKeyboardAndMouseInput)
+        {
+            isUsingMouse = false;
+            previousKeyboardAndMouseInput = keyboardAndMouseInput;
+        }
+
+        if(gun != null)
+        {
+            if (isUsingMouse == true)
+            {
+                rigB2D.velocity = movementSpeed * keyboardAndMouseInput * Time.deltaTime;
+            }
+            else
+            {
+                rigB2D.velocity = movementSpeed * gamepadController1Input * Time.deltaTime;
+            }
+        }
+    }
+}
