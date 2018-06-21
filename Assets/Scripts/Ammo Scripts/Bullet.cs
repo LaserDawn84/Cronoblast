c2b9530c840852ff89326df23a112c9b85d39b99
Lochlan Kennedy <1011533@student.sae.edu.au>
2018-06-21 17:08:43 +0800

    Adding Level Design Scripts

--- Assets/Scripts/Ammo Scripts/Bullet.cs
+++ Assets/Scripts/Ammo Scripts/Bullet.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿/*
+ *  Author: Lochlan Kennedy
+ *  Date: 11/06/2018
+ *  Version Control Repo: https://github.com/Pharah-tato/Cronoblast
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,20 +13,14 @@ public class Bullet : MonoBehaviour {
     GameObject bulletDestructionEffect;
     GameObject bulletDestructionEffectCopy;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        BulletDeath();
+    }
 
     void BulletDeath()
     {
         bulletDestructionEffectCopy = Instantiate(bulletDestructionEffect, gameObject.transform.position, gameObject.transform.rotation);
-        
-
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/Level Scripts/LayerSorter.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(SpriteRenderer))]
+public class LayerSorter : MonoBehaviour {
+
+    [SerializeField]
+    SpriteRenderer spriteRenderer;
+
+    [SerializeField]
+     int offset = 0; // bottom of the object
+
+
+	// Use this for initialization
+	void Awake () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        spriteRenderer.sortingOrder = (int)(-transform.position.y) + offset;
+		
+	}
+
+
+
+}
